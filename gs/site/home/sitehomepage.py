79d9000939e2b17c0ce65d05430818938920a772
Michael JasonSmith <mpj17@onlinegroups.net>
2012-02-13 16:40:04 +1100

    Initial import

---
+++ gs/site/home/sitehomepage.py
@@ -0,0 +1,8 @@
+ # coding=utf-8
+from zope.component import createObject
+from zope.cachedescriptors.property import Lazy
+from gs.content.base.page import SitePage
+
+class SiteHomepage(SitePage):
+    pass
+
