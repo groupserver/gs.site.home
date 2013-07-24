f70c82f5b6a9a861862cb04dbbdfd0a1061db4fd
Michael JasonSmith <mpj17@onlinegroups.net>
2013-07-24 14:21:16 -0700

    Whitespace change, for PEP-8.

--- gs/site/home/interfaces.py
+++ gs/site/home/interfaces.py
@@ -1,26 +1,30 @@
 # coding=utf-8
 from zope.viewlet.interfaces import IViewletManager
 
-# Metadata
+
 class ISiteHomeMetadata(IViewletManager):
     '''Viewlet manager for the metadata on the site homepage.'''
 
-# Body
+
 class ISiteHomeIntroduction(IViewletManager):
     '''The initial text, which is considered very important.'''
+
 
 class ISiteHomeMain(IViewletManager):
     '''The left column, which is normally used for static data.'''
 
+
 class ISiteHomeSecondary(IViewletManager):
     '''The left column, which is normally used for static data.'''
+
 
 class ISiteHomeActivity(IViewletManager):
     '''Some tabs, which are very similar to the Tabs shown on the Group page.'''
 
+
 class ISiteHomeEpilogue(IViewletManager):
     '''Unimportant information that appears at the bottom of the page.'''
 
-# JavaScript
+
 class ISiteHomeJS(IViewletManager):
     '''The JavaScript text for the site homepage'''
