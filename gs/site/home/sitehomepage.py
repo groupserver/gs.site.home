4de87b30389081737f4e2f779293e49b724d169b
Michael JasonSmith <mpj17@onlinegroups.net>
2014-03-26 12:44:38 -0700

    Updating an import

--- gs/site/home/sitehomepage.py
+++ gs/site/home/sitehomepage.py
@@ -1,8 +1,19 @@
- # coding=utf-8
-from zope.component import createObject
-from zope.cachedescriptors.property import Lazy
-from gs.content.base.page import SitePage
+ # -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2013, 2014 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from gs.content.base import SitePage
+
 
 class SiteHomepage(SitePage):
     pass
-
