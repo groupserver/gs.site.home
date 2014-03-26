331f41a3421f1658e37b2c8e26750c243112c281
Michael JasonSmith <mpj17@onlinegroups.net>
2014-03-26 12:44:46 -0700

    Turning off zip_safe, and updating the egg deps.

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2013, 2014 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
 import os
 from setuptools import setup, find_packages
 from version import get_version
@@ -29,15 +42,20 @@ setup(name='gs.site.home',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['gs', 'gs.site', ],
     include_package_data=True,
-    zip_safe=True,
+    zip_safe=False,
     install_requires=[
         'setuptools',
+        'zope.browserpage',
         'zope.cachedescriptors',
         'zope.component',
+        'zope.tal',
+        'zope.tales',
         'zope.viewlet',
+        'Zope2',
         'gs.content.base',
         'gs.content.layout',
         'gs.viewlet',
+        'Products.GSContent',
     ],
     entry_points="""
     # -*- Entry points: -*-
